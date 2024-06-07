d617468af38cba94529318cb52db344ca2a28bab
laithisbaitan <80163581+laithisbaitan@users.noreply.github.com>
2024-06-07 18:20:48 +0300

    changed endpoint

--- lib/utils/endpoints.dart
+++ lib/utils/endpoints.dart
@@ -1,8 +1,9 @@
 class Endpoints {
-  static const base = 'localhost:4000/api';
-  static const httpProtocol = 'http';
+  static const base = 'laith-chatapp.gigalixirapp.com/api';
+  static const httpProtocol = 'https';
   static const rooms = '$httpProtocol://$base/rooms';
   static String getMessages(String roomId) =>
       '$httpProtocol://$base/rooms/$roomId/chat';
-  static const websocket = 'ws://localhost:4000/socket/websocket';
+  static const websocket =
+      'ws://laith-chatapp.gigalixirapp.com/socket/websocket';
 }
